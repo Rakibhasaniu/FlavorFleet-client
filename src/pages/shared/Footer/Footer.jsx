b57e69221c97de0ebec639c85766d4c9bd3fcd24
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-09-26 19:29:57 +0600

    navbar and footer added

--- src/pages/shared/Footer/Footer.jsx
+++ src/pages/shared/Footer/Footer.jsx
@@ -59,7 +59,14 @@ const Footer = () => {
                         </a>
                     </div>
                 </nav>
+                
             </footer>
+            <footer className="footer footer-center bg-base-300 text-base-content p-4">
+  <aside>
+    <p>Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
+  </aside>
+</footer>
+
         </div>
     );
 };
