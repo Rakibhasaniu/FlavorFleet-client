62432e9394dc7625d7b3982a75d6bce8837da757
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-09-26 15:09:50 +0600

    layout created

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ function App() {
 
   return (
     <>
-      
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  
+
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+        <button className="btn btn-secondary">Ghost</button>
+
+      </h1>
+
 
     </>
   )
