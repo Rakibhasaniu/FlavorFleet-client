949c80708a30e5b50e48e34f296e6ed2d59bc9c3
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-10-25 18:50:29 +0600

    add order in cart

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,17 +1,50 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 
+import Swal from "sweetalert2";
 import useAuth from "../../hooks/useAuth";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 
 
 const FoodCard = ({item}) => {
-    const {image,recipe,price,name} = item;
+    const {image,recipe,price,name,_id} = item;
+    const navigate = useNavigate();
+    const location = useLocation();
 
     const {user} =useAuth()
-
+    console.log(user)
     const addToCart = (food) => {
-        console.log(food)
+        console.log(food,user)
+        if(user && user?.email){
+            const cartItem ={
+                menuId:_id,
+                email:user.email,
+                name,
+                image,
+                price,
+            }
+            axios.post('http://localhost:5000/cart',cartItem)
+            .then(res => {
+                console.log(res.data)
+            })
+        } else 
+        {
+            Swal.fire({
+                title: "You are not logged in",
+                text: "Please Log In to order",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, Login!"
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  navigate('/login',{state:{from:location}})
+                }
+              });
+        }
     }
 
     return (
