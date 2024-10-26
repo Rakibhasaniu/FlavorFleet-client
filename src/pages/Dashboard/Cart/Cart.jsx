f5c0fb36fbf849f74d790e238f53661b2dc21edb
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-10-26 15:24:15 +0600

    delete item from cart finished

--- src/pages/Dashboard/Cart/Cart.jsx
+++ src/pages/Dashboard/Cart/Cart.jsx
@@ -1,9 +1,41 @@
+import { FaTrash } from "react-icons/fa";
 import useCart from "../../../hooks/useCart";
+import Swal from "sweetalert2";
+import useAxios from "../../../hooks/useAxios";
 
 
 const Cart = () => {
-    const [cart] = useCart();
+    const [cart,refetch] = useCart();
     const totalPrice = cart.reduce((total, item) => total + item.price, 0)
+    const axiosSecure = useAxios();
+
+    const handleDelete = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                axiosSecure.delete(`/carts/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+
+            }
+        });
+    }
     return (
         <div>
             <div className="flex justify-evenly mb-8">
@@ -32,12 +64,12 @@ const Cart = () => {
                     </thead>
                     <tbody>
                         {
-                            cart.map((item,index) =>
+                            cart.map((item, index) =>
 
                                 <tr key={item._id}>
                                     <th>
                                         <label>
-                                            {index}
+                                            {index + 1}
                                         </label>
                                     </th>
                                     <td>
@@ -54,13 +86,16 @@ const Cart = () => {
                                     </td>
                                     <td>
                                         {item.name}
-                                        
+
                                     </td>
                                     <td>
                                         {item.price}
                                     </td>
                                     <th>
-                                        <button className="btn btn-ghost btn-xs">Delete</button>
+                                        <button onClick={() => handleDelete(item._id)} className="btn btn-ghost btn-lg">
+                                            <FaTrash className="text-red-600" />
+
+                                        </button>
                                     </th>
                                 </tr>
                             )
